59d78fb31d6afb1820808a4c4a71667b53d9ce3a
Gaurang Parekh <gaurang9rising@gmail.com>
2024-12-14 11:19:56 +0530

    login and registration part 1 and 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,16 +12,18 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $guarded = [];
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
 
     /**
      * The attributes that should be hidden for serialization.
